01ba266e0881b2540886ee59d3dedc517b912eff
Armond-Guze <Armond.Guze@yahoo.com>
2024-12-17 23:38:41 -0500

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
     return (
-        <BrowserRouter future={{ v7_relativeSplatPath: true }}>
-            <div className="relative z-0 bg-primary">
-                <Navbar />
-                <Routes>
-                    <Route path="/" />
-                    <Route path="/about"  />
-                    <Route path="/contact"  />
-                </Routes>
-            </div>
-        </BrowserRouter>
+      <BrowserRouter>
+        <div className='relative z-0 bg-primary'>
+          <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className='relative z-0'>
+            <Contact />
+            <StarsCanvas />
+          </div>
+        </div>
+      </BrowserRouter>
     );
-};
-
-export default App;
+  }
+  
+  export default App;
